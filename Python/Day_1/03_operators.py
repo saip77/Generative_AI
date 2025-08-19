6ea778b93b8e2f822ff2fcaa754032e79067d108
SAI PRASAD <sp12961369@gmail.com>
2025-08-20 02:05:12 +0530

    Operators

---
+++ Python/Day_1/03_operators.py
@@ -0,0 +1,37 @@
+# Operators in Python
+
+# Arithmetic Operators
+
+x = 10
+y = 3
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Floor Division:", x // y)
+print("Modulus:", x % y)
+print("Exponentiation:", x ** y)
+
+# Comparison Operators
+
+x = 10
+y = 3
+
+print("Equal to:", x == y)
+print("Not equal to:", x != y)
+print("Greater than:", x > y)
+print("Less than:", x < y)
+print("Greater than or equal to:", x >= y)
+print("Less than or equal to:", x <= y)
+
+# Logical Operators
+
+"""
+Note: In Python 1 2 3 "hi" [1,2,3] are all True
+      where as 0, " ", None, [] are all False
+"""
+x = True
+y = True
+
+print("And:", x and y)
