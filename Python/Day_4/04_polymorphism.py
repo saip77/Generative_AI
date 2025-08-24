c4a01b7189c953c3c168daf8c5fd876e391cff19
SAI PRASAD <sp12961369@gmail.com>
2025-08-24 20:55:31 +0530

    Polymorphism

--- Python/Day_4/04_polymorphism.py
+++ Python/Day_4/04_polymorphism.py
@@ -4,3 +4,28 @@
 Polymorphism means “many forms” — the same operation or method can behave differently 
 depending on the object that calls it.
 '''
+
+# Example of Polymorphism
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def sound(self):
+        print(f"Animal {self.name} makes a sound")
+    
+class Dog(Animal):
+    def sound(self):
+        print(f"Dog {self.name} barks")
+    
+class Cat(Animal):
+    def sound(self):
+        print(f"Cat {self.name} meows")
+
+a = Animal("Lion")
+a.sound()
+
+d = Dog("Tom")
+d.sound()
+
+c = Cat("Billu")
+c.sound()
