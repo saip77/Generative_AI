31fa8b88fc78f7d71991122e0d2d0ca3bdb17409
SAI PRASAD <sp12961369@gmail.com>
2025-08-25 22:49:56 +0530

    Python problem solving

---
+++ Python_Problem_Solving/Day_1/01_problem.py
@@ -0,0 +1,15 @@
+#Grocery Bill Calculator
+
+'''Input price of 3 grocery items.
+Print the total bill and also the average cost per item. '''
+
+item_1 = float(input("Price of item 1: "))
+item_2 = float(input("Price of item 2: "))
+item_3 = float(input("Price of item 3: "))
+
+total = item_1 + item_2 + item_3
+
+average_price = total / 3
+
+print(f"Total Bill: ${total:.2f}")
+print(f"Average Price per Item: ${average_price:.2f}")
---
+++ Python_Problem_Solving/Day_1/02_problem.py
@@ -0,0 +1,12 @@
+#Movie Ticket Booking
+
+'''Input user’s name, age, and movie name.
+Print a formatted ticket:
+"Hello Sai, you booked a ticket for Avatar. Age: 24"
+'''
+
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+movie_name = input("Enter the movie name: ")
+
+print(f"Hello {user_name}, you booked a ticket for {movie_name}. Age: {user_age}")
